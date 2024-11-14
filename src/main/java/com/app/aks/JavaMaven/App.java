c98f4fc7025a7a28879365af9a058368eb636263
Akshay <akshaygunti19@gmail.com>
2024-11-14 12:19:36 +0530

    sample java maven

---
+++ src/main/java/com/app/aks/JavaMaven/App.java
@@ -0,0 +1,11 @@
+package com.app.aks.JavaMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("AKSHAY");
+        System.out.println("22bd1a054j");
+    }
+}
